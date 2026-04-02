5fcd4dd377d51207ec418ea65c59bee64737acae
Jamik <jamik@jamshed.com>
2026-04-02 18:54:26 +0500

    go

--- src/component/box.jsx
+++ src/component/box.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 
 class Box extends React.Component {
-  render(props) {
-    return (                
-       <div className="w-[270px] h-[330px] ">
-        <img src={props.img} width={70} alt="" />
-        <h1 className="text-[24px] font-bold">{props.title}</h1>
-        <p className="w-[270px] text-[#2D3436] text-[18px]">{props.description}</p>
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="sm:w-[270px] w-[150px] sm:h-[330px] h-[373px] flex flex-col  ">
+        <div className="w-[70px] h-[70px] rounded-full flex items-center justify-center bg-green-600">
+          <img src={this.props.img} width={70} alt="" />
+        </div>
+        <h1 className="text-[24px] font-bold">{this.props.title}</h1>
+        <p className=" text-[#2D3436] text-[18px]">
+          {this.props.description}
+        </p>
         <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
           Add to Cart
         </button>
-       </div>
+      </div>
     );
   }
 }
--- src/component/sectionBox.jsx
+++ src/component/sectionBox.jsx
@@ -4,9 +4,20 @@ import phone from "../assets/phone.png";
 class SectionBox extends React.Component {
   render() {
     return (
-      <>
-    <Box img={phone} title="Product Title" description="Product Description" />
-      </>
+        <section className=" max-w-[1400px] m-auto flex flex-col        gap-10 mt-20 mb-20">
+
+            <div >
+                <h1 className="sm:text-[32px] text-[24px]">Delivering good designs since 1954 🚚💨</h1>
+                <p className="sm:text-[24px] w-[900px] text-[18px]">We’re the first multi-purpose design kit solutions for businesses. We help you bridge
+gaps between your layouts, templates and developers to empower all involved.</p>
+            </div>
+      <div className="flex  justify-between flex-wrap  ">
+    <Box img={phone} title="Product Title" description="Delivering faster and more personalized support with shared screens and cool design systems for Figma" />
+    <Box img={phone} title="Product Title" description="Delivering faster and more personalized support with shared screens and cool design systems for Figma" />
+    <Box img={phone} title="Product Title" description="Delivering faster and more personalized support with shared screens and cool design systems for Figma" />
+    <Box img={phone} title="Product Title" description="Delivering faster and more personalized support with shared screens and cool design systems for Figma" />
+      </div>
+                    </section>
     );
   }
 }
